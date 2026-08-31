79cf90651ac69ac153ff7cdb94ef876b2d1bc351
Rizki Pratama Sunarko <rizkypratama16322@gmail.com>
2026-08-31 08:12:46 +0700

    menghilangkan tombol login admin

--- src/komponen/refound/ItemStatus.tsx
+++ src/komponen/refound/ItemStatus.tsx
@@ -9,5 +9,4 @@ interface ItemStatusProps {
 
 export default function ItemStatus({ tipe, statusLaporan }: ItemStatusProps) {
   return <LencanaStatus tipe={tipe} statusLaporan={statusLaporan} />;
-}
-export default function MatchCard({tipe, statusLaporan}) 
+}
